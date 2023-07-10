2c5ea8208437377a1add36ea366351c130562a87
Joseph Hill <joseph.hill1@outlook.com>
2023-07-10 15:53:40 -0400

    Cleaned up config file.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,4 @@
-import type {GatsbyConfig} from "gatsby";
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -6,35 +6,40 @@ const config: GatsbyConfig = {
     description: `An example site using Gatsby`,
     twitterUsername: `@jhill`,
     image: `./src/images/icon.png`,
-    siteUrl: `https://examplemain.gatsbyjs.io/`
+    siteUrl: `https://examplemain.gatsbyjs.io/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "04MTsw8EUCKJS6gXIwmmpum2VlxWyCYnczso8OTQfCg",
-      "spaceId": "nzrnovga5lru"
-    }
-
-    }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-mdx",
+  plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-contentful",
       options: {
-        "name": "pages",
-        "path": `${__dirname}/src/pages/`
-      }
+        accessToken: "04MTsw8EUCKJS6gXIwmmpum2VlxWyCYnczso8OTQfCg",
+        spaceId: "nzrnovga5lru",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages/`,
+      },
     },
     {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            "name": "blog",
-            "path": `${__dirname}/blog/`
-        }
-    }
-  ]
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/blog/`,
+      },
+    },
+  ],
 };
 
 export default config;
